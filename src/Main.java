76583019fb39b110de3deffead2a97c2c4016b97
nane <rayvieiraaa@gmail.com>
2023-05-26 09:55:46 -0300

    Aula muito legal

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,25 @@ public class Main {
     // Definir comportamento do objeto
         qualquer.falar();
         adao.falar("baixo");
+        // Criar mais objetos
+        Pessoa rainha;
+
+        //Instanciar objetos
+        rainha = new Pessoa();
+
+        //Definir formato do objeto
+        rainha.nome = "Tina";
+        rainha.sobrenome = "Tunner";
+
+        //Definir comportamento
+        rainha.falar();
+        rainha.falar("alto");
+        System.out.println(rainha.falar("alto", "grave"));
+
+        Animal cao = new Animal();
+        cao.nome = "Pluto";
+        cao.comer();
+
+        rainha.comer();
 }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,13 +1,26 @@
-public class Pessoa {
+public class Pessoa extends Animal {
     //Atributo
-    String nome;
     String sobrenome;
 
     //Metodos de classe
     public void falar(){
         System.out.println("falei");
     }
+
     public void falar(String volume){
         System.out.println("falei " + volume);
     }
+
+    //Sobrecarga de método
+    public void falar(String volume) {
+        System.out.println("falei " + volume);
+    }
+        public String falar(String volume, String tom){
+            return "falei " + volume;
+        }
+        //Sobrescrita do método
+    public void comer() {
+        System.out.println("Comer como pessoa");
+    }
+}
 }
